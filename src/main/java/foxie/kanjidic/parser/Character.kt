24e9cc45893afa1d452859e47387417ceea70535
WinteryFox <dragonhel5@gmail.com>
2019-10-22 14:37:47 +0200

    works so far

--- src/main/java/foxie/kanjidic/parser/Character.kt
+++ src/main/java/foxie/kanjidic/parser/Character.kt
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText
 
 data class Character(
-        var cp_value: CpValue
+        var literal: String,
+        @JacksonXmlElementWrapper
+        var codepoint: List<CpValue>
 )
 
 data class CpValue(
-        @JacksonXmlText
-        var value: String,
-        @JacksonXmlProperty(isAttribute = true, localName = "cp_type")
+        @JacksonXmlProperty(isAttribute = true)
         var cp_type: String
-)
+) {
+        @JacksonXmlText
+        lateinit var cp_value: String private set
+}
