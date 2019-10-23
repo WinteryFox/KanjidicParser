2d9dd9f03b028d7c1b79f6b1a98eb8b5c21ad24f
WinteryFox <dragonhel5@gmail.com>
2019-10-23 18:43:08 +0200

    drop indices before creating tables

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,5 +1,23 @@
 BEGIN;
 
+DROP INDEX IF EXISTS character_index;
+
+DROP INDEX IF EXISTS codepoint_index;
+
+DROP INDEX IF EXISTS radical_index;
+
+DROP INDEX IF EXISTS miscellaneous_index;
+
+DROP INDEX IF EXISTS dictionary_index;
+
+DROP INDEX IF EXISTS code_index;
+
+DROP INDEX IF EXISTS reading_index;
+
+DROP INDEX IF EXISTS meaning_index;
+
+DROP INDEX IF EXISTS nanori_index;
+
 DROP TABLE IF EXISTS character CASCADE;
 CREATE TABLE character
 (
@@ -22,7 +40,7 @@ CREATE TABLE radical
     character INT  NOT NULL REFERENCES character (id),
     type      TEXT NOT NULL,
     radical   INT  NOT NULL,
-    PRIMARY KEY (character, radical)
+    PRIMARY KEY (character, type)
 );
 
 DROP TABLE IF EXISTS miscellaneous;
@@ -45,8 +63,8 @@ CREATE TABLE dictionary
     dictionary TEXT NOT NULL,
     volume     INT,
     page       INT,
-    value      TEXT  NOT NULL,
-    PRIMARY KEY (character, dictionary, value)
+    value      TEXT NOT NULL,
+    PRIMARY KEY (character, dictionary)
 );
 
 DROP TABLE IF EXISTS code;
@@ -64,7 +82,7 @@ CREATE TABLE reading
     character INT  NOT NULL REFERENCES character (id),
     type      TEXT NOT NULL,
     reading   TEXT NOT NULL,
-    PRIMARY KEY (character, type, reading)
+    PRIMARY KEY (character, type)
 );
 
 DROP TABLE IF EXISTS meaning;
@@ -73,15 +91,14 @@ CREATE TABLE meaning
     character INT  NOT NULL REFERENCES character (id),
     language  TEXT NOT NULL,
     reading   TEXT NOT NULL,
-    PRIMARY KEY (character, language, reading)
+    PRIMARY KEY (character, language)
 );
 
 DROP TABLE IF EXISTS nanori;
 CREATE TABLE nanori
 (
-    character INT  NOT NULL REFERENCES character (id),
-    nanori    TEXT NOT NULL,
-    PRIMARY KEY (character, nanori)
+    character INT  NOT NULL REFERENCES character (id) PRIMARY KEY,
+    nanori    TEXT NOT NULL
 );
 
 END;
