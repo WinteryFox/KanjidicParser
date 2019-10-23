1190f753f5fd63c94c7d24be9ddd2a154ad9434e
WinteryFox <dragonhel5@gmail.com>
2019-10-23 14:59:10 +0200

    encapsulate everything into use

--- src/main/java/foxie/kanjidic/parser/Main.kt
+++ src/main/java/foxie/kanjidic/parser/Main.kt
@@ -27,57 +27,60 @@ fun main(args: Array<String>) {
             .readValue(file, Kanjidic::class.java)
 
     println("Inserting into PostgreSQL...")
-    val connection = DriverManager
+    DriverManager
             .getConnection("jdbc:postgresql://${args[0]}/${args[1]}?user=${args[2]}&password=${args[3]}")
+            .use { connection ->
+                connection.createStatement().use {
+                    it.execute(Main::class.java.getResource("/tables.sql").readText())
+                }
 
-    connection.createStatement()
-            .execute(Main::class.java.getResource("/tables.sql").readText())
+                connection.createStatement().use { statement ->
+                    kanjidic.characters.forEach { character ->
+                        statement.addBatch("INSERT INTO character (literal) VALUES ('${character.literal}') ON CONFLICT DO NOTHING")
 
-    val statement = connection.createStatement()
+                        character.codepoint.forEach {
+                            statement.addBatch("INSERT INTO codepoint (character, type, codepoint) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
+                        }
 
-    kanjidic.characters.forEach { character ->
-        statement.addBatch("INSERT INTO character (literal) VALUES ('${character.literal}') ON CONFLICT DO NOTHING")
+                        character.radical.forEach {
+                            statement.addBatch("INSERT INTO radical (character, type, radical) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
+                        }
 
-        character.codepoint.forEach {
-            statement.addBatch("INSERT INTO codepoint (character, type, codepoint) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
-        }
+                        statement.addBatch("INSERT INTO miscellaneous (character, grade, stroke_count, frequency, variant_type, variant, jlpt, radical_name) VALUES (lastval(), ${character.misc.grade}, ${character.misc.strokeCount}, ${character.misc.frequency}, ${character.misc.variant?.type.asSql()}, ${character.misc.variant?.value.asSql()}, ${character.misc.jlpt}, ${character.misc.radicalName.asSql()}) ON CONFLICT DO NOTHING")
 
-        character.radical.forEach {
-            statement.addBatch("INSERT INTO radical (character, type, radical) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
-        }
+                        character.references?.forEach {
+                            statement.addBatch("INSERT INTO dictionary (character, dictionary, volume, page, value) VALUES (lastval(), ${it.type.asSql()}, ${it.volume}, ${it.page}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
+                        }
 
-        statement.addBatch("INSERT INTO miscellaneous (character, grade, stroke_count, frequency, variant_type, variant, jlpt, radical_name) VALUES (lastval(), ${character.misc.grade}, ${character.misc.strokeCount}, ${character.misc.frequency}, ${character.misc.variant?.type.asSql()}, ${character.misc.variant?.value.asSql()}, ${character.misc.jlpt}, ${character.misc.radicalName.asSql()}) ON CONFLICT DO NOTHING")
+                        character.codes?.forEach {
+                            statement.addBatch("INSERT INTO code (character, type, code) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
+                        }
 
-        character.references?.forEach {
-            statement.addBatch("INSERT INTO dictionary (character, dictionary, volume, page, value) VALUES (lastval(), ${it.type.asSql()}, ${it.volume}, ${it.page}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
-        }
+                        character.readingMeaning?.group?.meanings?.forEach {
+                            statement.addBatch("INSERT INTO meaning (character, language, reading) VALUES (lastval(), ${if (it.lang == null) "en".asSql() else it.lang.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
+                        }
 
-        character.codes?.forEach {
-            statement.addBatch("INSERT INTO code (character, type, code) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
-        }
+                        character.readingMeaning?.group?.readings?.forEach {
+                            statement.addBatch("INSERT INTO reading (character, type, reading) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
+                        }
 
-        character.readingMeaning?.group?.meanings?.forEach {
-            statement.addBatch("INSERT INTO meaning (character, language, reading) VALUES (lastval(), ${if (it.lang == null) "en".asSql() else it.lang.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
-        }
+                        character.readingMeaning?.nanori?.forEach {
+                            statement.addBatch("INSERT INTO nanori (character, nanori) VALUES (lastval(), ${it.asSql()}) ON CONFLICT DO NOTHING")
+                        }
+                    }
 
-        character.readingMeaning?.group?.readings?.forEach {
-            statement.addBatch("INSERT INTO reading (character, type, reading) VALUES (lastval(), ${it.type.asSql()}, ${it.value.asSql()}) ON CONFLICT DO NOTHING")
-        }
+                    try {
+                        statement.executeBatch()
+                    } catch (e: BatchUpdateException) {
+                        e.nextException?.printStackTrace()
+                    }
+                }
 
-        character.readingMeaning?.nanori?.forEach {
-            statement.addBatch("INSERT INTO nanori (character, nanori) VALUES (lastval(), ${it.asSql()}) ON CONFLICT DO NOTHING")
-        }
-    }
-
-    try {
-        statement.executeBatch()
-    } catch (e: BatchUpdateException) {
-        e.nextException?.printStackTrace()
-    }
-
-    println("Creating indices...")
-    connection.createStatement()
-            .execute(Main::class.java.getResource("/indices.sql").readText())
+                println("Creating indices...")
+                connection.createStatement().use { statement ->
+                    statement.execute(Main::class.java.getResource("/indices.sql").readText())
+                }
+            }
 
     println("Done!")
 }
